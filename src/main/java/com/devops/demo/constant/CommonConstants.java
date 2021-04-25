cbee4f80c645e0c14a56dc3186079ca8c430e386
wangfenglei <wangfenglei@eversec.cn>
2021-04-25 15:09:32 +0800

    adjust

--- src/main/java/com/devops/demo/constant/CommonConstants.java
+++
@@ -1,9 +0,0 @@
-package com.devops.demo.constant;
-
-/**
- * @author john
- */
-public class CommonConstants {
-    public static final String JWT_KEY_USER = "currentUser";
-    public static final String ACCESS_TOKEN = "accessToken";
-}
